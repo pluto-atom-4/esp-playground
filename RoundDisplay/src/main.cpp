176025e5c71436203e71122d48adf7877ed2b457
NT <it.scope@gmail.com>
2025-09-28 17:58:37 -0700

    Refactor setup function for improved serial output and initialization

--- RoundDisplay/src/main.cpp
+++ RoundDisplay/src/main.cpp
@@ -7,11 +7,17 @@ uint32_t count;
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("Starting setup...");
+  Serial.printf("SCLK:%d MOSI:%d MISO:%d DC:%d CS:%d\n", D8, D10, D9, D3, D1);
   lcd.init();
+  Serial.println("LCD initialized.");
 }
 
 void loop()
 {
+ // Serial.print("Loop count: ");
+ // Serial.println(count);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.printf("%d\r\n", count++);
